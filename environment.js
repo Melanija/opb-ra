c1b5f2853e0171b4d89054e36945883d656aef91
Melanija <vezocnik.melanija@gmail.com>
2026-09-21 15:07:55 +0200

    minor updates

--- environment.js
+++ environment.js
@@ -94,7 +94,7 @@ function loadDomain(domain) {
 }
 
 function prepare() {
-    document.getElementById('baza').value = 'facebooktwitter';
+    document.getElementById('baza').value = 'facebookx';
     document.getElementById('baza').onchange = function () {
         let value = this.value;
         loadDomain(value);
