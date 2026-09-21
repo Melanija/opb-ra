cdbce3ff6b4f624696e8be0456e27f0d08a9a366
Melanija <vezocnik.melanija@gmail.com>
2026-09-21 15:17:53 +0200

    minor updates

---
+++ facebookx_simple.js
@@ -0,0 +1,24 @@
+relations = [
+    {
+        name: 'facebook', header: ['OID', 'PID'], types: ['string', 'string'], shortName: 'f',
+        data: [
+            ['Jill', 'Jack'],
+            ['Jack', 'Jill'],
+            ['Jack', 'Joe'],
+            ['Joe', 'Jack'],
+            ['Jack', 'Jenn'],
+            ['Jenn', 'Jack'],
+            ['Joe', 'Jenn'],
+            ['Jenn', 'Joe']
+        ]
+    },
+    {
+        name: 'omrezjeX', header: ['OID', 'SID'], types: ['string', 'string'], shortName: 'x',
+        data: [
+            ['Jill', 'Jack'],
+            ['Joe', 'Jack'],
+            ['Jenn', 'Joe'],
+            ['Jack', 'Jenn']
+        ]
+    }
+]
